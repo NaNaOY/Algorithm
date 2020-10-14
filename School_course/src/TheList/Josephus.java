4107a2dc1549a474a4f7fa0bd83d37c9d2d45080
niuhongwei <nhw1013@outlook.com>
2020-10-14 18:30:46 +0800

    链表

---
+++ School_course/src/TheList/Josephus.java
@@ -0,0 +1,39 @@
+package TheList;
+
+public class Josephus {
+    //n个人，n>0;从start开始计数，0<=start<n;每次数到distant的人出环，0<distance<n;
+    public Josephus(int n,int start,int distance){
+        if(n<=0||start<0||start>=n||distance<=0||distance>=n){
+            throw new IllegalArgumentException("n="+n+",start="+start+",distance="+distance);
+        }
+        SinglyLinkedList<String> list=new SinglyLinkedList<String>();
+
+        for(int i=0;i<=n;i++){
+            Node<String> node =new Node<String>((char)('A'+i-1)+"");
+            list.addNode(node);
+        }
+        System.out.println("Josephus("+n+","+start+"，"+distance+")");
+        Node<String> front=list.head;
+        for(int i=0;front!=null&&i<start;i++){
+            front=front.next;
+        }
+        list.print();
+        while(n>1){
+            for(int i=0;i<distance;i++){
+                front=front.next;
+                if(front==null){
+                    front=list.head.next;
+                }
+            }
+            if(front.next==null) {
+                front = list.head;
+            }
+                System.out.println("删除"+front.next.data.toString()+",");
+                front.next=front.next.next;
+                n--;
+                list.print();
+        }
+        System.out.print("被赦免者是");
+        list.print();
+    }
+}
