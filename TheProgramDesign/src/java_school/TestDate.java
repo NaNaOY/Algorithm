f5b4ad0eff05eb792d933ef59ec7eda2f8c0e03d
niuhongwei <nhw1013@outlook.com>
2020-10-21 08:55:05 +0800

    date

--- TheProgramDesign/src/java_school/TestDate.java
+++ TheProgramDesign/src/java_school/TestDate.java
@@ -1,24 +1,81 @@
 package java_school;
 
+import java.util.Calendar;
 import java.util.Scanner;
+import java.util.Date;
 
 public class TestDate {
+private int year,day,month;
 
+public int getYear(){
+    return year;
+}
 
-    public static void main(String[] args) {
-// 定义年份
-        int year = -1;
-// 定义月份du
-        int month = -1;
+public int getMonth(){
+    return month;
+}
+
+public int getDay(){
+    return day;
+}
+
+public void setMonth(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("请输入月");
+    month = scanner.nextInt();
+}
+
+public void setYear(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("请输入年");
+    year = scanner.nextInt();
+}
+
+    public void setDay(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入年");
-        year = scanner.nextInt();
-        System.out.println("请输入月");
-        month = scanner.nextInt();
-        System.out.println(year + "年" + month + "月有" + days(year, month) + "天");
-
+        System.out.println("请输入天");
+        day = scanner.nextInt();
     }
 
+    public String toString() {
+        return year+"-"+month+"-"+day;
+    }
+    public void showDate() {
+        System.out.println(toString());
+    }
+
+    public void moveForward(){
+        Date date = new Date();//取时间
+        Calendar calendar  =   Calendar.getInstance();
+
+        calendar.setTime(date); //需要将date数据转移到Calender对象中操作
+        calendar.add(calendar.DATE, -1);//把日期往前增加1天.正数往后推,负数往前移动
+        date=calendar.getTime();   //这个时间就是日期往后推一天的结果
+    }
+    public void moveForward(int n){
+        Date date = new Date();//取时间
+        Calendar calendar  =   Calendar.getInstance();
+
+        calendar.setTime(date); //需要将date数据转移到Calender对象中操作
+        calendar.add(calendar.DATE, -n);//把日期往前增加1天.正数往后推,负数往前移动
+        date=calendar.getTime();   //这个时间就是日期往后推一天的结果
+    }
+    public void moveNext(){
+        Date date = new Date();//取时间
+        Calendar calendar  =   Calendar.getInstance();
+
+        calendar.setTime(date); //需要将date数据转移到Calender对象中操作
+        calendar.add(calendar.DATE, 1);//把日期往后增加1天.正数往后推,负数往前移动
+        date=calendar.getTime();   //这个时间就是日期往后推一天的结果
+    }
+    public void moveNext(int n){
+        Date date = new Date();//取时间
+        Calendar calendar  =   Calendar.getInstance();
+
+        calendar.setTime(date); //需要将date数据转移到Calender对象中操作
+        calendar.add(calendar.DATE, n);//把日期往后增加1天.正数往后推,负数往前移动
+        date=calendar.getTime();   //这个时间就是日期往后推一天的结果
+    }
     public static int days(int year, int month) {
         int days = 0;
         if (month != 2) {
@@ -49,6 +106,19 @@ public class TestDate {
 
         }
         return days;
+    }
+
+
+
+    public static void main(String[] args){
+    TestDate date=new TestDate();
+    date.setDay();
+    date.setMonth();
+    date.setYear();
+    date.showDate();
+    date.moveForward();
+    date.showDate();
+
     }
 }
 
